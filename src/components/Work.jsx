fbb6e58f191938dfe73e2eb8bbb7826ef359fa6c
+ <khalifnashon@gmail.com>
2023-05-23 13:49:49 +0300

    work component done

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,3 +1,5 @@
+import Sendit from "../assets/projects/Sendit.png"
+
 const Work = () => {
   return (
     <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
@@ -5,22 +7,146 @@ const Work = () => {
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">Work</p>
-          <p>// Check out some of my recent work</p>
+          <p className ="py-6">// Check out some of my recent work</p>
         </div>
 
         {/* Project container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
 
           {/* Specific card item */}
-          <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-style-div">
-            <div>
-              <span></span>
+          <div style={{backgroundImage: `url(${Sendit})`}}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+            
+            {/* Hover effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider ">
+                React JS Application
+              </span>
               <div>
                 <a href="/">
-                  <button></button>
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
+                    Code
+                  </button>
                 </a>
                 <a href="/">
-                  <button></button>
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Live
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Specific card item */}
+          <div style={{backgroundImage: `url(${Sendit})`}}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+            
+            {/* Hover effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider ">
+                React JS Application
+              </span>
+              <div>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
+                    Code
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Live
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Specific card item */}
+          <div style={{backgroundImage: `url(${Sendit})`}}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+            
+            {/* Hover effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider ">
+                React JS Application
+              </span>
+              <div>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
+                    Code
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Live
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Specific card item */}
+          <div style={{backgroundImage: `url(${Sendit})`}}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+            
+            {/* Hover effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider ">
+                React JS Application
+              </span>
+              <div>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
+                    Code
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Live
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Specific card item */}
+          <div style={{backgroundImage: `url(${Sendit})`}}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+            
+            {/* Hover effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider ">
+                React JS Application
+              </span>
+              <div>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
+                    Code
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Live
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Specific card item */}
+          <div style={{backgroundImage: `url(${Sendit})`}}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+            
+            {/* Hover effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider ">
+                React JS Application
+              </span>
+              <div>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
+                    Code
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Live
+                  </button>
                 </a>
               </div>
             </div>
